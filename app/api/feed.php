be7d3361a901d619da8491187b2f80ef7a96f872
Lucas Holt <luke@foolishgames.com>
2014-03-13 22:23:03 -0400

    add the rss feed

--- app/api/feed.php
+++ app/api/feed.php
@@ -1,12 +1,37 @@
-<?php 
+<?php
+function gzip_compression() {
+
+    //If no encoding was given - then it must not be able to accept gzip pages
+    if( empty($_SERVER['HTTP_ACCEPT_ENCODING']) ) { return false; }
+
+    //If zlib is not ALREADY compressing the page - and ob_gzhandler is set
+    if (( ini_get('zlib.output_compression') == 'On'
+    	OR ini_get('zlib.output_compression_level') > 0 )
+    	OR ini_get('output_handler') == 'ob_gzhandler' ) {
+    	return false;
+    }
+
+    //Else if zlib is loaded start the compression.
+    if ( extension_loaded( 'zlib' ) AND (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== FALSE) ) {
+    	ob_start('ob_gzhandler');
+    }
+
+    return true;
+}
+
+gzip_compression();
+
 header('Content-type: application/rss+xml');
 mb_http_output("iso-8859-1");
-echo '<?xml version="1.0" encoding="ISO-8859-1" ?>';
+
+include( 'dbinfo.php' );
+
+echo '<?xml version="1.0" encoding="ISO-8859-1" ?>' . "\n";
 ?>
 <rss version="2.0">
 	<channel>
 		<title>Jewel News Feed</title>
-     	<link>http://www.foolishgames.com/jewel/news/</link>
+        <link>http://www.foolishgames.com/#/news</link>
    		<description>Foolish Games Jewel News</description>
 		<language>en-us</language>
 		<webMaster>luke@foolishgames.com</webMaster>
@@ -15,8 +40,6 @@ echo '<?xml version="1.0" encoding="ISO-8859-1" ?>';
 		<docs>http://blogs.law.harvard.edu/tech/rss</docs>
 		<ttl>360</ttl>
 <?php
-include( '../../scripts/dbinfo.php' );
-   
 $currentPage = $_SERVER["PHP_SELF"];
 
 $conn = mysqli_connect( $dbhost, $dbuser, $dbpass, $dbname );
@@ -44,7 +67,7 @@ if ($myrow = mysqli_fetch_array($result, MYSQLI_BOTH)) {
 	echo $subject;
 	echo "</title>";
     
-	echo "<link>http://www.foolishgames.com/jewel/news/index#";
+	echo "<link>http://www.foolishgames.com/#/news/";
 	echo $myrow["id"]; 
 	echo "</link>";
 	
@@ -54,19 +77,12 @@ if ($myrow = mysqli_fetch_array($result, MYSQLI_BOTH)) {
 	echo "</item>";
 
   } while ($myrow = mysqli_fetch_array($result));
-  
-  echo "</channel>";
-  echo "</rss>";
-
-} else {
-
-	echo "Sorry, no records were found!";	
-
 }
 
 mysqli_free_result($result);
 
 /* close connection */
 mysqli_close($conn);
-
 ?>
+</channel>
+</rss>
