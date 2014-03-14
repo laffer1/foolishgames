ec61af1a1c3bb9a587d1ce5f35e680d4da1aca03
Lucas Holt <luke@foolishgames.com>
2014-03-13 22:06:38 -0400

    add gzip

--- app/api/news.php
+++ app/api/news.php
@@ -1,8 +1,30 @@
 <?php
+function gzip_compression() {
+
+    //If no encoding was given - then it must not be able to accept gzip pages
+    if( empty($_SERVER['HTTP_ACCEPT_ENCODING']) ) { return false; }
+
+    //If zlib is not ALREADY compressing the page - and ob_gzhandler is set
+    if (( ini_get('zlib.output_compression') == 'On'
+    	OR ini_get('zlib.output_compression_level') > 0 )
+    	OR ini_get('output_handler') == 'ob_gzhandler' ) {
+    	return false;
+    }
+
+    //Else if zlib is loaded start the compression.
+    if ( extension_loaded( 'zlib' ) AND (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== FALSE) ) {
+    	ob_start('ob_gzhandler');
+    }
+
+    return true;
+}
+
+gzip_compression();
+
 header('Content-type: application/json');
 mb_http_output("iso-8859-1");
 
-include('../scripts/dbinfo.php');
+include('dbinfo.php');
 
 class NewsItem
 {
@@ -12,14 +34,22 @@ class NewsItem
     public $content;
 }
 
+$maxRows_rs1 = 10;
+$pageNum_rs1 = 0;
+if (isset($_GET['pg'])) {
+  $pageNum_rs1 = $_GET['pg'];
+}
+$startRow_rs1 = $pageNum_rs1 * $maxRows_rs1;
+
+$query_rs1 = "SELECT id, DATE_FORMAT(date, '%Y-%m-%dT%TZ') as date, title, content FROM articles ORDER BY date DESC, id DESC";
+$sql = sprintf("%s LIMIT %d, %d", $query_rs1, $startRow_rs1, $maxRows_rs1);
+
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 if (!$conn) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
-$sql = "SELECT id, DATE_FORMAT(date, '%Y-%m-%dT%TZ') as date, title, content FROM articles ORDER BY date DESC, id DESC";
 
 $arr = array();
 $result = mysqli_query($conn, $sql);
