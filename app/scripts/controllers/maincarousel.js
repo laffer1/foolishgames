79917982c4626a6c41ca1bf24ff7e9c0d0603893
Lucas Holt <luke@foolishgames.com>
2014-03-13 20:49:51 -0400

    try a different rotation out

--- app/scripts/controllers/maincarousel.js
+++ app/scripts/controllers/maincarousel.js
@@ -5,18 +5,23 @@ angular.module('foolishgamesApp')
 
             $scope.slides = [
                 {
+                    image: 'http://www.foolishgames.com/images/pics/high_school/hs_2.jpg',
+                    width: 350,
+                    text: 'Jewel in High School',
+                    active: true
+                },
+                {
                     image: 'http://www.foolishgames.com/images/jewelroof.jpg',
                     width: 300,
                     text: 'Jewel',
                     active: true
-                }   ,
+                },
                 {
-                    image: 'http://www.foolishgames.com/images/jewelroof.jpg',
-                    width: 300,
+                    image: 'http://www.foolishgames.com/images/pics/013102/013102gaycrisis01.jpg',
+                    width: 240,
                     text: '',
                     active: true
                 }
             ];
-
         }]
-);
+        );
