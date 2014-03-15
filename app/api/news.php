b9692877d13bdc8cc400d85720718a284d356a57
Lucas Holt <luke@foolishgames.com>
2014-03-15 09:48:49 -0400

    add support for pulling a single news article.

--- app/api/news.php
+++ app/api/news.php
@@ -1,19 +1,23 @@
 <?php
-function gzip_compression() {
+function gzip_compression()
+{
 
     //If no encoding was given - then it must not be able to accept gzip pages
-    if( empty($_SERVER['HTTP_ACCEPT_ENCODING']) ) { return false; }
+    if (empty($_SERVER['HTTP_ACCEPT_ENCODING'])) {
+        return false;
+    }
 
     //If zlib is not ALREADY compressing the page - and ob_gzhandler is set
-    if (( ini_get('zlib.output_compression') == 'On'
-    	OR ini_get('zlib.output_compression_level') > 0 )
-    	OR ini_get('output_handler') == 'ob_gzhandler' ) {
-    	return false;
+    if ((ini_get('zlib.output_compression') == 'On'
+            OR ini_get('zlib.output_compression_level') > 0)
+        OR ini_get('output_handler') == 'ob_gzhandler'
+    ) {
+        return false;
     }
 
     //Else if zlib is loaded start the compression.
-    if ( extension_loaded( 'zlib' ) AND (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== FALSE) ) {
-    	ob_start('ob_gzhandler');
+    if (extension_loaded('zlib') AND (strpos($_SERVER['HTTP_ACCEPT_ENCODING'], 'gzip') !== FALSE)) {
+        ob_start('ob_gzhandler');
     }
 
     return true;
@@ -34,16 +38,6 @@ class NewsItem
     public $content;
 }
 
-$maxRows_rs1 = 10;
-$pageNum_rs1 = 0;
-if (isset($_GET['pg'])) {
-  $pageNum_rs1 = $_GET['pg'];
-}
-$startRow_rs1 = $pageNum_rs1 * $maxRows_rs1;
-
-$query_rs1 = "SELECT id, DATE_FORMAT(date, '%Y-%m-%dT%TZ') as date, title, content FROM articles ORDER BY date DESC, id DESC";
-$sql = sprintf("%s LIMIT %d, %d", $query_rs1, $startRow_rs1, $maxRows_rs1);
-
 $conn = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 if (!$conn) {
@@ -51,23 +45,53 @@ if (!$conn) {
     exit();
 }
 
-$arr = array();
-$result = mysqli_query($conn, $sql);
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
 
-if ($myrow = mysqli_fetch_array($result, MYSQLI_BOTH)) {
-    do {
-        $item = new NewsItem();
-        $item->title = $myrow["title"];
-        $item->content = $myrow["content"];
-        $item->id = $myrow["id"];
-        $item->date = $myrow["date"];
+    if (is_int($id) && $id > 0) {
+        $sql = "SELECT id, DATE_FORMAT(date, '%Y-%m-%dT%TZ') AS date, title, content FROM articles WHERE id=$id LIMIT 1";
+        $result = mysqli_query($conn, $sql);
 
-        array_push($arr, $item);
-    } while ($myrow = mysqli_fetch_array($result));
+        if ($myrow = mysqli_fetch_array($result, MYSQLI_BOTH)) {
+            $item = new NewsItem();
+            $item->title = $myrow["title"];
+            $item->content = $myrow["content"];
+            $item->id = $myrow["id"];
+            $item->date = $myrow["date"];
+
+            echo json_encode($item);
+        }
+
+        mysqli_free_result($result);
+    }
+} else {
+    $maxRows_rs1 = 10;
+    $pageNum_rs1 = 0;
+    if (isset($_GET['pg'])) {
+        $pageNum_rs1 = $_GET['pg'];
+    }
+    $startRow_rs1 = $pageNum_rs1 * $maxRows_rs1;
+
+    $query_rs1 = "SELECT id, DATE_FORMAT(date, '%Y-%m-%dT%TZ') AS date, title, content FROM articles ORDER BY date DESC, id DESC";
+    $sql = sprintf("%s LIMIT %d, %d", $query_rs1, $startRow_rs1, $maxRows_rs1);
+
+    $arr = array();
+    $result = mysqli_query($conn, $sql);
+
+    if ($myrow = mysqli_fetch_array($result, MYSQLI_BOTH)) {
+        do {
+            $item = new NewsItem();
+            $item->title = $myrow["title"];
+            $item->content = $myrow["content"];
+            $item->id = $myrow["id"];
+            $item->date = $myrow["date"];
+
+            array_push($arr, $item);
+        } while ($myrow = mysqli_fetch_array($result));
+    }
+    mysqli_free_result($result);
+
+    echo json_encode($arr);
 }
 
-mysqli_free_result($result);
 mysqli_close($conn);
-
-echo json_encode($arr);
-?>
