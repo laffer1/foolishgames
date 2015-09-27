e9121192f8aaef73754b4c52a9c190b3c3accf97
Lucas Holt <luke@foolishgames.com>
2015-09-27 16:02:15 -0400

    add picking up the pieces image.

--- app/scripts/controllers/maincarousel.js
+++ app/scripts/controllers/maincarousel.js
@@ -1,21 +1,27 @@
 angular.module('foolishgamesApp')
-        .controller('MainCarouselCtrl', ['$scope', function ($scope) {
-            'use strict';
-            $scope.myInterval = 5000;
+  .controller('MainCarouselCtrl', ['$scope', function ($scope) {
+    'use strict';
+    $scope.myInterval = 5000;
 
-            $scope.slides = [
-                {
-                    image: 'http://www.foolishgames.com/images/jewelroof.jpg',
-                    width: 300,
-                    text: 'Jewel',
-                    active: true
-                },
-                {
-                    image: 'http://www.foolishgames.com/images/pics/013102/013102gaycrisis01.jpg',
-                    width: 240,
-                    text: '',
-                    active: true
-                }
-            ];
-        }]
-        );
+    $scope.slides = [
+      {
+        image: 'http://www.foolishgames.com/images/pics/pickingupthepieces/pickingupthepieces_small.png',
+        width: 300,
+        text: '',
+        active: true
+      },
+      {
+        image: 'http://www.foolishgames.com/images/jewelroof.jpg',
+        width: 300,
+        text: 'Jewel',
+        active: true
+      },
+      {
+        image: 'http://www.foolishgames.com/images/pics/013102/013102gaycrisis01.jpg',
+        width: 240,
+        text: '',
+        active: true
+      }
+    ];
+  }]
+);
