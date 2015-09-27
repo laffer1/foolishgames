09bedfb01cf60581525c236429384239af0b5dcf
Lucas Holt <luke@foolishgames.com>
2015-09-27 16:44:17 -0400

    add more images to the slideshow

--- app/scripts/controllers/maincarousel.js
+++ app/scripts/controllers/maincarousel.js
@@ -23,6 +23,30 @@ angular.module('foolishgamesApp')
         active: true
       },
       {
+        image: 'http://www.foolishgames.com/images/pics/rso/jewel2z.jpg',
+        width: 315,
+        text: '',
+        active: true
+      },
+      {
+        image: 'http://www.foolishgames.com/images/pics/rso/jewel4z.jpg',
+        width: 335,
+        text: '',
+        active: true
+      },
+      {
+        image: 'http://www.foolishgames.com/images/pics/photoshoot2/photoshoot1.jpg',
+        width: 300,
+        text: '',
+        active: true
+      },
+      {
+        image: 'http://www.foolishgames.com/images/pics/photoshoot2/jewel_052.jpg',
+        width: 292,
+        text: '',
+        active: true
+      },
+      {
         image: 'http://www.foolishgames.com/images/pics/013102/013102gaycrisis01.jpg',
         width: 240,
         text: '',
